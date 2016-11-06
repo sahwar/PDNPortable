3a577ea6284e5482978b9e0445e9f65cb42433f3
toehead2001 <toe_head2001@yahoo.com>
2016-11-05 17:23:50 -0700

    Allow images to be dragged and dropped onto the exe
    Closes #1

--- PDNPortable/Program.cs
+++ PDNPortable/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Win32;
 using System.IO;
 using System.Diagnostics;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace PDNPortable
 {
@@ -43,9 +44,25 @@ namespace PDNPortable
             regKey.SetValue("CHECKFORUPDATES", "0", RegistryValueKind.String);
 
             // Start paint.net, and wait for it to close
-            string pdnexe = @"paint.net\PaintDotNet.exe";
+            string pdnexe = Application.StartupPath + @"\paint.net\PaintDotNet.exe";
+
+            string imagePaths = string.Empty;
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                List<string> imageList = new List<string>();
+                for (int i = 1; i < args.Length; i++)
+                {
+                    if (File.Exists(args[i]))
+                        imageList.Add("\"" + args[i] + "\"");
+                }
+
+                if (imageList.Count > 0)
+                    imagePaths = string.Join(" ", imageList);
+            }
+
             if (File.Exists(pdnexe))
-                Process.Start(pdnexe).WaitForExit();
+                Process.Start(pdnexe, imagePaths).WaitForExit();
             else
                 MessageBox.Show("Can not find the paint.net executable. Please copy it from an existing installation.", "PDN Portable");
 
